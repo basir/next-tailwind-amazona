7f41ffd4aac4df9e39da97b35c0ee82061c1942f
Basir <basir.jafarzadeh@gmail.com>
2022-04-28 14:32:51 +0430

    Video-05-Create-Website-Layout

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ decription
 2. Install Tools
 3. Create Next App
 4. Publish to Github
+5. Create Website Layout
+   1. create layout component
+   2. add header
+   3. add main section
+   4. add footer
+   5. add tailwind classes
---
+++ components/Layout.js
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+
+export default function Layout({ title, children }) {
+  return (
+    <>
+      <Head>
+        <title>{title ? title + ' - Amazona' : 'Amazona'}</title>
+        <meta name="description" content="Ecommerce Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="flex min-h-screen flex-col justify-between ">
+        <header>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
+            <Link href="/">
+              <a className="text-lg font-bold">amazona</a>
+            </Link>
+            <div>
+              <Link href="/cart">
+                <a className="p-2">Cart</a>
+              </Link>
+              <Link href="/login">
+                <a className="p-2">Login</a>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          <p>Copyright © 2022 Amazona</p>
+        </footer>
+      </div>
+    </>
+  );
+}
