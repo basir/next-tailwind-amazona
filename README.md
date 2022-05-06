f9c5a74d9f0496d94abafa213bb78bacbc48a0aa
Basir <basir.jafarzadeh@gmail.com>
2022-05-06 14:30:29 +0430

    Video-07-Create-Product-Details-Page

--- README.md
+++ README.md
@@ -18,3 +18,10 @@ decription
    1. add data.js
    2. add images
    3. render products
+7. Create Product Details
+   1. create product page
+   2. create 3 columns
+   3. show image in first column
+   4. show product info in second column
+   5. show add to cart action on third column
+   6. add styles
---
+++ pages/product/[slug].js
@@ -0,0 +1,59 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import Layout from '../../components/Layout';
+import data from '../../utils/data';
+
+export default function ProductScreen() {
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find((x) => x.slug === slug);
+  if (!product) {
+    return <div>Produt Not Found</div>;
+  }
+  return (
+    <Layout title={product.name}>
+      <div className="py-2">
+        <Link href="/">back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          ></Image>
+        </div>
+        <div>
+          <ul>
+            <li>
+              <h1 className="text-lg">{product.name}</h1>
+            </li>
+            <li>Category: {product.category}</li>
+            <li>Brand: {product.brand}</li>
+            <li>
+              {product.rating} of {product.numReviews} reviews
+            </li>
+            <li>Description: {product.description}</li>
+          </ul>
+        </div>
+        <div>
+          <div className="card p-5">
+            <div className="mb-2 flex justify-between">
+              <div>Price</div>
+              <div>${product.price}</div>
+            </div>
+            <div className="mb-2 flex justify-between">
+              <div>Status</div>
+              <div>{product.countInStock > 0 ? 'In stock' : 'Unavailable'}</div>
+            </div>
+            <button className="primary-button w-full">Add to cart</button>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
