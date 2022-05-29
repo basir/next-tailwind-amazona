b79a96c8c5cad2864be9914a10d5a78dec9b248c
Basir <basir.jafarzadeh@gmail.com>
2022-05-29 11:40:33 +0430

    Video-18-Seed-Sample-Products

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ decription
 17. Create Payment Method Screen
     1. dispaly payment methods
     2. save payment method in context
+18. Seed sample products
+    1. insert sample products to mongodb
+    2. load products from db in home and product screen
+    3. check product count in stock in add to cart
--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,3 +1,4 @@
+import Product from '../../models/Product';
 import User from '../../models/User';
 import data from '../../utils/data';
 import db from '../../utils/db';
@@ -6,6 +7,8 @@ const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
+  await Product.deleteMany();
+  await Product.insertMany(data.products);
   await db.disconnect();
   res.send({ message: 'seeded successfully' });
 };
