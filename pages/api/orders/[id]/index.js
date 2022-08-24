f1d7f08ab4277854790e8876bdab3acfb7f38409
chisomprince <chisomprince17@gmail.com>
2022-08-24 15:39:33 +0100

    ...

--- pages/api/orders/[id]/index.js
+++ pages/api/orders/[id]/index.js
@@ -1,13 +1,13 @@
 // /api/orders/:id
-import { getSession } from 'next-auth/react';
-import Order from '../../../../models/Order';
-import db from '../../../../utils/db';
+import { getSession } from "next-auth/react";
+import Order from "../../../../models/Order";
+import db from "../../../../utils/db";
 
 const handler = async (req, res) => {
   const session = await getSession({ req });
-  if (!session) {
-    return res.status(401).send('signin required');
-  }
+  // if (!session) {
+  //   return res.status(401).send('signin required');
+  // }
 
   await db.connect();
 
